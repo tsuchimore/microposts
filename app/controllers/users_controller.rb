25b7c6136025be0ad9dec06df98cf900c5fe9116
tsuchimore1 <yoshi1003to216@gmail.com>
2016-02-11 13:57:43 +0000

    add user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,23 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update]
   def edit
+    unless current_user == @user
+      redirect_to root_path
+    end     
   end
   
   def update
-    if @user.update(user_params)
+ 
+    if current_user == @user
+      if @user.update(user_params)
       # 保存に成功した場合はトップページへリダイレクト
       redirect_to root_path , notice: 'メッセージを編集しました'
-    else
+      else
       # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
+      end
+    else
+      redirect_to root_path
     end
   end
   def show
