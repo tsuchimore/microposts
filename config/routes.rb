c9f81086a656a37b13f5cb321fa62a7f2a8a95e4
tsuchimore1 <yoshi1003to216@gmail.com>
2016-02-11 10:38:18 +0000

    add user model

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get    'signup', to: 'users#new'
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
+  get  'Settings', to: 'users#edit'
   delete 'logout', to: 'sessions#destroy'
 
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160207073829) do
+ActiveRecord::Schema.define(version: 20160211005758) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160207073829) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "age"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
